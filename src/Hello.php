231ae3e6a02aa169b000c0325e696de4690b26b0
zhangjiaxiang3 <zhangjiaxiang3@wanda.cn>
2018-02-28 23:15:13 +0800

    composer.test.first commit

---
+++ src/Hello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangjiaxiang
+ * Date: 2018/2/28
+ * Time: 下午10:23
+ */
+
+namespace Hello;
+
+class Hello
+{
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangjiaxiang
+ * Date: 2018/2/28
+ * Time: 下午10:30
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+use Hello\Hello;
+echo Hello::world();
